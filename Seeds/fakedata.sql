48075ec20c981fdbd31dd3f62606e32e45107040
Nathan Bowdish <djnatronic@gmail.com>
2020-06-11 21:03:56 -0700

    updated seeds fakedata to be activies

--- Seeds/fakedata.sql
+++ Seeds/fakedata.sql
@@ -2,6 +2,6 @@ INSERT INTO users (first, last, username, passsword) values ('Jane', 'Austen','j
 INSERT INTO users (first, last, username, passsword) values ('John', 'Doe','john@github.com','123456');
 INSERT INTO users (first, last, username, passsword) values ('Clark', 'Kent','clark@github.com','123456');
 
-INSERT INTO Activities (User_id, Trip_ID, Country, City, Activity_1,Activity_2,Activity_3,Activity_4,Activity_5) values ('1', '1', 'Russia','Moscow','Red Square', 'Something Cool2','Something Cool3','Something Cool4','Something Cool5');
-INSERT INTO Activities (User_id, Trip_ID, Country, City, Activity_1,Activity_2,Activity_3,Activity_4,Activity_5) values ('2', '2', 'USA','Seattle','Space Needle', 'Something Cool2','Something Cool3','Something Cool4','Something Cool5');
-INSERT INTO Activities (User_id, Trip_ID, Country, City, Activity_1,Activity_2,Activity_3,Activity_4,Activity_5) values ('3', '3','France','Paris','Eiffel Tower', 'Something Cool2','Something Cool3','Something Cool4','Something Cool5');
+INSERT INTO activities (User_id, Trip_ID, Country, City, Activity_1,Activity_2,Activity_3,Activity_4,Activity_5) values ('1', '1', 'Russia','Moscow','Red Square', 'Something Cool2','Something Cool3','Something Cool4','Something Cool5');
+INSERT INTO activities (User_id, Trip_ID, Country, City, Activity_1,Activity_2,Activity_3,Activity_4,Activity_5) values ('2', '2', 'USA','Seattle','Space Needle', 'Something Cool2','Something Cool3','Something Cool4','Something Cool5');
+INSERT INTO activities (User_id, Trip_ID, Country, City, Activity_1,Activity_2,Activity_3,Activity_4,Activity_5) values ('3', '3','France','Paris','Eiffel Tower', 'Something Cool2','Something Cool3','Something Cool4','Something Cool5');
