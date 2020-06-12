bf782dfcbadcff77b86c73c877c657e1e793dc31
jeffreyadamo <jeffreyadamo@gmail.com>
2020-06-11 21:33:43 -0700

    added Trip_ID to activities.js, working on html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,82 +26,75 @@ module.exports = function(app) {
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+
+  // ===================================================
+  // ===================================================
+  // Need Routes for setting up API calls from the database
+  // ===================================================
+  // ===================================================
+
+  // Wikipedia
+  // ==================================================
+
+  // Unsplash Pictures
+  // ==================================================
+  let country;
+  pictureArray = [];
+
+  function upSplashAJAX(country) {
+    const picURL =
+      "https://api.unsplash.com/search/photos?client_id=l_ucLpuaVqeosGc7xD0pKg6Ib61kn737l_M3-nkFmZY&query=" +
+      country;
+    $.ajax({
+      url: picURL,
+      method: "GET"
+    }).then(results => {
+      const countryPic = results.results[0].urls.small;
+      pictureArray.push(countryPic);
+    });
+  }
+
+  app.get("/members/:id", (req, res) => {
+    //get
+    db.activities
+      .findOne({
+        where: {
+          // eslint-disable-next-line camelcase
+          User_id: req.params.id
+        }
+      })
+      .then(dbactivities => {
+        console.log(res.json(dbactivities));
+        country = res.json(dbactivities.country);
+        upSplashAJAX(country);
+      });
+  });
 };
-
-
-// ===================================================
-// ===================================================
-// Need Routes for setting up API calls from the database
-// ===================================================
-// ===================================================
-
-// Wikipedia
-// ==================================================
-
-// Unsplash Pictures
-// ==================================================
-var country;
-picture_array = [];
-
-function upSplashAJAX(country) {
-  var Picurl = 'https://api.unsplash.com/search/photos?client_id=l_ucLpuaVqeosGc7xD0pKg6Ib61kn737l_M3-nkFmZY&query=' + country;
-  $.ajax({
-    url: Picurl,
-    method: "GET"
-  })
-  .then(function (results) {
-    var countryPic = results.results[0].urls.small
-    picture_array.push(countryPic)
-  })
-}
-
-
-// app.get("/members/:id", function (req, res){
-//   //get 
-//   db.Itineraries.findOne({
-//     where: {
-//       User_id: req.params.id
-//     }
-//   }).then(function(dbItineraries){
-//     console.log(res.json(dbItineraries))
-//     country = res.json(dbItineraries.country)
-//     upSplashAJAX(country)
-  
-//   })
-// })
-
-
 // Exchange rate calculator
 // ==================================================
 
-
-
 // Open Trip Map
 // ==================================================
-
 
 // app.get("/", (req, res) => {
 //   // If the user already has an account send them to the members page
 //   if (req.user) {
 //     res.redirect("/members");
 //   }
-//   // Static HTML 
+//   // Static HTML
 //   res.sendFile(path.join(__dirname, "../public/signup.html"));
 
 //   // Needs to be converted to Handlebars.js
 
-
 // });
 
-
 // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  // app.get("/members", isAuthenticated, (req, res) => {
+// If a user who is not logged in tries to access this route they will be redirected to the signup page
+// app.get("/members", isAuthenticated, (req, res) => {
 
-  //   // Statis HTML
-  //   res.sendFile(path.join(__dirname, "../public/members.html"));
+//   // Statis HTML
+//   res.sendFile(path.join(__dirname, "../public/members.html"));
 
-  //   // Needs to be converted to Handlebars.js
+//   // Needs to be converted to Handlebars.js
 
-
-  // });
+// });
