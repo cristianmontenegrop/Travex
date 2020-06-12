042d91985c60ec97d210d0d481c3dfaeec4f494b
Cris M <cristian.montenegro.p@gmail.com>
2020-06-12 02:55:19 -0700

    database connected to jaws

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 // Requiring necessary npm packages
 const express = require("express");
-const session = require('express-session');
+const session = require("express-session");
 // Requiring passport as we've configured it
-const passport = require('./config/passport');
+const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
